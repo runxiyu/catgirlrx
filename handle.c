98b76d1b7efe54d03b4c20a3f28d78116c2abb07
Curtis McEnroe <june@causal.agency>
2019-07-27 18:00:01 -0400

    Make copyright headers consistent

--- handle.c
+++ handle.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2018, 2019  Curtis McEnroe <june@causal.agency>
+/* Copyright (C) 2018, 2019  C. McEnroe <june@causal.agency>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
