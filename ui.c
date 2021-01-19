1fbc8fe3948a739a2101e0e70af1e0049b8e7837
Jeremy O'Brien <neutral@fastmail.com>
2021-01-19 11:52:43 -0500

    Don't lose swapped window when navigating to current buffer

--- ui.c
+++ ui.c
@@ -733,9 +733,10 @@ static void inputUpdate(void) {
 }
 
 static void windowShow(uint num) {
+	windows.user = num;
+	if (windows.show == num) return;
 	windows.swap = windows.show;
 	windows.show = num;
-	windows.user = num;
 	mark(windows.ptrs[windows.swap]);
 	unmark(windows.ptrs[windows.show]);
 	windowUpdate();
