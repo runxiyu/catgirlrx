b106440752c1c2c786642a6b656a92738715f9ff
C. McEnroe <june@causal.agency>
2021-05-27 15:53:02 +0000

    Set username from SSH_CLIENT in chroot

--- scripts/chroot-prompt.sh
+++ scripts/chroot-prompt.sh
@@ -3,5 +3,5 @@ set -eu
 
 printf 'Name: '
 read -r nick rest
-printf '%s %s\n' "$nick" "${SSH_CLIENT:-}" >>nicks.log
-exec catgirl -n "$nick" -s "$nick" "$@"
+printf '%s %s\n' "$nick" "$SSH_CLIENT" >>nicks.log
+exec catgirl -K -n "$nick" -s "$nick" -u "${SSH_CLIENT%% *}" "$@"
