78a020df8242ae5e4e2ac210bc8d85dc27b43bb6
C. McEnroe <june@causal.agency>
2021-07-20 21:27:24 -0400

    chroot: Avoid passing paths to -s

--- scripts/chroot-prompt.sh
+++ scripts/chroot-prompt.sh
@@ -4,4 +4,4 @@ set -eu
 printf 'Name: '
 read -r nick rest
 printf '%s %s\n' "$nick" "$SSH_CLIENT" >>nicks.log
-exec catgirl -K -n "$nick" -s "$nick" -u "${SSH_CLIENT%% *}" "$@"
+exec catgirl -K -n "$nick" -s "${nick##*/}" -u "${SSH_CLIENT%% *}" "$@"
